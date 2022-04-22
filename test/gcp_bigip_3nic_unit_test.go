0da94a956103762469e2d78fa35f40113137f7ae
RavinderReddyF5 <r.chinthalapalli@f5.com>
2022-04-22 21:25:13 +0530

    adding test changes

--- test/gcp_bigip_3nic_unit_test.go
+++ test/gcp_bigip_3nic_unit_test.go
@@ -1,8 +1,13 @@
 package test
 
 import (
+	"crypto/tls"
+	"fmt"
+	"strings"
 	"testing"
+	"time"
 
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,7 +21,7 @@ func TestTerraformGCP3NicExample(t *testing.T) {
 		TerraformDir: "../examples/bigip_gcp_3nic_deploy",
 
 		Vars: map[string]interface{}{
-			"region" : "us-central1",
+			"region": "us-central1",
 		},
 	}
 
@@ -24,16 +29,48 @@ func TestTerraformGCP3NicExample(t *testing.T) {
 
 	terraform.InitAndApply(t, terraformOptions)
 
-	mgmtPublicIP := terraform.Output(t, terraformOptions, "mgmtPublicIP")
-	bigipPassword := terraform.Output(t, terraformOptions, "bigip_password")
-	bigipUsername := terraform.Output(t, terraformOptions, "bigip_username")
-	mgmtPort := terraform.Output(t, terraformOptions, "mgmtPort")
-	mgmtPublicURL := terraform.Output(t, terraformOptions, "mgmtPublicURL")
+	mgmtPublicIP := strings.Trim(terraform.Output(t, terraformOptions, "mgmtPublicIP"), "[]")
+	bigipPassword := strings.Trim(terraform.Output(t, terraformOptions, "bigip_password"), "[]")
+	bigipUsername := strings.Trim(terraform.Output(t, terraformOptions, "bigip_username"), "[]")
+	mgmtPort := strings.Trim(terraform.Output(t, terraformOptions, "mgmtPort"), "[]")
 
-	assert.NotEqual(t, "", mgmtPublicIP[0])
-	assert.NotEqual(t, "", bigipPassword[0])
-	assert.NotEqual(t, "", bigipUsername[0])
-	assert.Equal(t, "443", mgmtPort[0])
-	assert.NotEqual(t, "", mgmtPublicURL[0])
+	assert.NotEqual(t, "", mgmtPublicIP)
+	assert.NotEqual(t, "", bigipPassword)
+	assert.Equal(t, "bigipuser", bigipUsername)
+	assert.Equal(t, "443", mgmtPort)
 
+	as3Url := fmt.Sprintf("https://%s:%s@%s:%s/mgmt/shared/appsvcs/info", bigipUsername, bigipPassword, mgmtPublicIP, mgmtPort)
+	doUrl := fmt.Sprintf("https://%s:%s@%s:%s/mgmt/shared/declarative-onboarding/info", bigipUsername, bigipPassword, mgmtPublicIP, mgmtPort)
+	// as3Url := fmt.Sprintf("https://%s:%s@%s:%s/mgmt/shared/appsvcs/info", bigipUsername, bigipPassword, mgmtPublicIP, mgmtPort)
+
+	// Setup a TLS configuration to submit with the helper, a blank struct is acceptable
+	tlsConfig := tls.Config{
+		InsecureSkipVerify: true,
+	}
+
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		as3Url,
+		&tlsConfig,
+		10,
+		10*time.Second,
+		verifyAs3Resp,
+	)
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		doUrl,
+		&tlsConfig,
+		10,
+		10*time.Second,
+		verifyDoResp,
+	)
+}
+func verifyAs3Resp(statusCode int, body string) bool {
+	var respStr = `{"version":"3.34.0","release":"4","schemaCurrent":"3.34.0","schemaMinimum":"3.0.0"}`
+	return statusCode == 200 && strings.Contains(body, respStr)
+}
+
+func verifyDoResp(statusCode int, body string) bool {
+	var respStr = `"version":"1.27.0"`
+	return statusCode == 200 && strings.Contains(body, respStr)
 }
